1901f7255e2ea6e7a3ec9814e4d3cafe2768d0ef
fghdhttdsffgyhd-ops <fghdhttdsffgyhd@gmail.com>
2026-04-30 04:26:46 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,10 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // المسار الصحيح لضمان عمل الروابط والملفات بشكل صحيح على مسار GitHub Pages
+  // المسار الصحيح للنشر على GitHub Pages
   base: '/seera-alex2026/',
   
   plugins: [react()],
-  
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: true,
-    hmr: {
-      clientPort: 443,
-    }
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
